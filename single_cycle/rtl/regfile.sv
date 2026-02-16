e942110c5103aeca4250b08e99ebe16ef524e0c6
Chat <chathil.rajaman3@gmail.com>
2026-02-16 01:42:55 -0800

    regfile in progress

--- single_cycle/rtl/regfile.sv
+++ single_cycle/rtl/regfile.sv
@@ -9,3 +9,22 @@ module dff_reg (clk, in, out);
         out <= in;
     end
 endmodule
+
+module regfile (clk, in, out);
+    parameter BIT_WIDTH = 32;
+
+    input   wire   clk;
+    input   wire   [BIT_WIDTH-1:0] in;
+
+    output  reg    [BIT_WIDTH-1:0] out;
+
+    wire [BIT_WIDTH-1:0] x [15:0];
+
+    always_ff @(posedfe clk) begin
+        
+
+    end
+    
+
+
+endmodule
