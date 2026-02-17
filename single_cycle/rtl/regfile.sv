bccdda7f05fee05597a17b7e3a9870868ba40ac5
Chat <63841542+ChillZero@users.noreply.github.com>
2026-02-16 18:25:26 -0800

    regfile in progress

--- single_cycle/rtl/regfile.sv
+++ single_cycle/rtl/regfile.sv
@@ -10,21 +10,30 @@ module dff_reg (clk, in, out);
     end
 endmodule
 
-module regfile (clk, in, out);
+module regfile (clk, data_in, write_en, write_reg, read_reg, data_out);
     parameter BIT_WIDTH = 32;
 
-    input   wire   clk;
-    input   wire   [BIT_WIDTH-1:0] in;
+    input   wire   clk;                     //clk signal
+    input   wire   [BIT_WIDTH-1:0] data_in; //data in
+    input   wire   write_en;                //write enable
+    input   wire   [3:0] write_reg;         //register to be written
+    input   wire   [3:0] read_reg;          //register to be read
 
-    output  reg    [BIT_WIDTH-1:0] out;
 
-    wire [BIT_WIDTH-1:0] x [15:0];
+    output  reg    [BIT_WIDTH-1:0] data_out;//data output bus
 
-    always_ff @(posedfe clk) begin
-        
+    reg [BIT_WIDTH-1:0] x [15:0];          //create registers
 
+    assign x[0] = `0;
+
+    always_ff @(posedge clk) begin
+        //write to reg if write_en high
+        if (write_en) begin
+            x[write_reg] <= data_in;
+        end
     end
-    
 
 
+
+    assign data_out = x[read_reg];          //data output is at read_reg value
 endmodule
